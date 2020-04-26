4c24bf74c788ef430edfd28db4085799ae646f51
Artem Sharapov <frezzy12349@gmail.com>
2020-04-26 18:15:10 +0300

    initial commit

---
+++ OperationSystems-Multithreading/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OperationSystems-Multithreading
+//
+//  Created by Артем Шарапов on 26.04.2020.
+//  Copyright © 2020 Hooba-booba. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
